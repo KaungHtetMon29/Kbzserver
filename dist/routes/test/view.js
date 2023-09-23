5a28e8c8d8a5f6be2cd1cef22fd16729d985f722
KaungHtetMon29 <99006407+KaungHtetMon29@users.noreply.github.com>
2023-09-23 12:28:53 +0630

    Delete dist directory

--- dist/routes/test/view.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var controller_1 = require("./controller");
-var express = require('express');
-var router = express.Router();
-router.post("/", controller_1.add);
-exports.default = router;
-//# sourceMappingURL=view.js.map
