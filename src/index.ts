32eed6f299b63e15e71b827073310bc116dcf508
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-09-24 21:44:10 +0630

    test

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,21 @@
-import AppDataSource  from "./db/data-source"
-import routes from "./routes"
+import AppDataSource from "./db/data-source";
+import routes from "./routes";
 require("dotenv").config();
-const express=require('express')
-const cors=require('cors')
-const app=express();
-const port =process.env.USER;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+// const port =process.env.;
 
-AppDataSource.initialize().then(async () => {
-    console.log("success")
+AppDataSource.initialize()
+  .then(async () => {
+    console.log("success");
+  })
+  .catch((error) => console.log(error));
+app.use(express.json({ limit: "10mb" }));
 
-}).catch(error => console.log(error))
-app.use(express.json({ limit: '10mb' }));
-
-app.use(express.json())
-app.use(cors())
-app.use("/",routes)
+app.use(express.json());
+app.use(cors());
+app.use("/", routes);
 app.listen(5000, () => {
-    console.log(`Server is running ${port}`);
-  });
+  console.log(`Server is running 5000`);
+});
