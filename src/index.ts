377c1f0ee2c8c9abf8d1591fbcc71442484bdbab
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-09-25 03:42:12 +0630

    test

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 // const port =process.env.;
-
+const port = process.env.PORT || 5000;
 AppDataSource.initialize()
   .then(async () => {
     console.log("success");
@@ -16,6 +16,6 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.json());
 app.use(cors());
 app.use("/", routes);
-app.listen(5000, () => {
-  console.log(`Server is running 5000`);
+app.listen(port, () => {
+  console.log(`Server is running ${port}`);
 });
