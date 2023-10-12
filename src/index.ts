dd307cfb013fb15353959d76ed284ef1952b705b
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-10-12 12:31:08 +0630

    keep alive

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,10 @@ AppDataSource.initialize()
   })
   .catch((error) => console.log(error));
 app.use(express.json({ limit: "10mb" }));
-
+const keepalive = () => {
+  console.log("running");
+};
+setInterval(keepalive, 10 * 60 * 1000);
 app.use(express.json());
 app.use(cors());
 app.use("/", routes);
