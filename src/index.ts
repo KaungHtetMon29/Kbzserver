251a2bb4f964f6a9f6ef1bc20f0c60acee250029
KaungHtetMon29 <kaunghtetmon456@gmail.com>
2023-10-12 13:12:43 +0630

    running every 14 mins

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ app.use(express.json({ limit: "10mb" }));
 const keepalive = () => {
   console.log("running");
 };
-setInterval(keepalive, 10 * 60 * 1000);
+setInterval(keepalive, 14 * 60 * 1000);
 app.use(express.json());
 app.use(cors());
 app.use("/", routes);
